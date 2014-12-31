a192eb12577bbc369606a42a63a13ef947fb0cc7
Donnerbart <github@thunderphreak.de>
2014-12-31 19:33:11 +0100

    Got rid of RandomAccessFile in FileHandlerEventProducer.java

--- src/main/java/FileHandlerEventProducer.java
+++ src/main/java/FileHandlerEventProducer.java
@@ -1,9 +1,9 @@
 import com.lmax.disruptor.RingBuffer;
 
 import java.io.IOException;
-import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
 import java.util.concurrent.CountDownLatch;
 
 public class FileHandlerEventProducer
@@ -19,33 +19,30 @@ public class FileHandlerEventProducer
 
 	public void produce(String fileName)
 	{
-		try (RandomAccessFile aFile = new RandomAccessFile(fileName, "r"))
+		try (FileChannel fileChannel = FileChannel.open(Paths.get(fileName)))
 		{
-			try (FileChannel inChannel = aFile.getChannel())
+			ByteBuffer buffer = ByteBuffer.allocate(2);
+			while (fileChannel.read(buffer) > 0)
 			{
-				ByteBuffer buffer = ByteBuffer.allocate(2);
-				while (inChannel.read(buffer) > 0)
+				buffer.flip();
+				boolean isPadded = (buffer.limit() < 2);
+				int char1 = buffer.get();
+				int char2 = (isPadded) ? ' ' : buffer.get();
+
+				long sequence = ringBuffer.next();
+				try
 				{
-					buffer.flip();
-					boolean isPadded = (buffer.limit() < 2);
-					int char1 = buffer.get();
-					int char2 = (isPadded) ? ' ' : buffer.get();
-
-					long sequence = ringBuffer.next();
-					try
-					{
-						FileReaderEvent fileReaderEvent = ringBuffer.get(sequence);
-						fileReaderEvent.setChar1(char1);
-						fileReaderEvent.setChar2(char2);
-						fileReaderEvent.setPadded(isPadded);
-					}
-					finally
-					{
-						ringBuffer.publish(sequence);
-					}
-
-					buffer.clear();
+					FileReaderEvent fileReaderEvent = ringBuffer.get(sequence);
+					fileReaderEvent.setChar1(char1);
+					fileReaderEvent.setChar2(char2);
+					fileReaderEvent.setPadded(isPadded);
 				}
+				finally
+				{
+					ringBuffer.publish(sequence);
+				}
+
+				buffer.clear();
 			}
 		}
 		catch (IOException e)
